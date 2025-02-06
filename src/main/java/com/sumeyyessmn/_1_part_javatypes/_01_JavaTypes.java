9db2e677aa8a33053ecfb4dd74dcbb6b74d2bbee
Sumeyye Sisman <sumyssmn@gmail.com>
2025-02-06 18:39:25 +0300

    class, psvm, sout

--- src/main/java/com/sumeyyessmn/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/sumeyyessmn/_1_part_javatypes/_01_JavaTypes.java
@@ -1,4 +1,9 @@
 package com.sumeyyessmn._1_part_javatypes;
 
 public class _01_JavaTypes {
-}
+    public static void main(String[] args) {
+       //Çıktı: sout
+        System.out.println("Sumeyye Sisman");
+
+    } //end psvm
+} //end class
