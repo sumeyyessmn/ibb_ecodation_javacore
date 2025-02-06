0b95a10745be2f60cf93252eebb34841100c5bbf
Sumeyye Sisman <sumyssmn@gmail.com>
2025-02-06 19:04:00 +0300

    primitive

--- src/main/java/com/sumeyyessmn/_1_part_javatypes/_01_JavaPsvm.java
+++ src/main/java/com/sumeyyessmn/_1_part_javatypes/_01_JavaPsvm.java
@@ -1,6 +1,6 @@
 package com.sumeyyessmn._1_part_javatypes;
 
-public class _01_JavaTypes {
+public class _01_JavaPsvm {
     public static void main(String[] args) {
        //Çıktı: sout
         System.out.println("Sumeyye Sisman");
---
+++ src/main/java/com/sumeyyessmn/_1_part_javatypes/_02_JavaPrimitiveTypes.java
@@ -0,0 +1,31 @@
+package com.sumeyyessmn._1_part_javatypes;
+
+public class _02_JavaPrimitiveTypes {
+
+    public static void main(String[] args) {
+        //Primitive Types: 8 tanedir. null deger alamazsiniz
+        //tam sayilar (4)
+        byte b1 = 127;
+        System.out.println("byte: "+b1);
+        short s1 = 32767;
+        System.out.println("short: "+s1);
+        int i1 = 2464632;
+        System.out.println("int: "+i1);
+        long l1 = 35768437823L;
+        System.out.println("long: "+l1);
+        //virgullu sayilar (floating point)
+        float f2 = 3.14f;
+        System.out.println("float: "+f2);
+        double d2 = 3.14;
+        System.out.println("double: "+d2);
+        //boolean
+        boolean b3 = true;
+        System.out.println("boolean: "+b3);
+        //char
+        char c4 = 'c';
+        System.out.println("char: "+c4);
+
+
+
+    }
+}
