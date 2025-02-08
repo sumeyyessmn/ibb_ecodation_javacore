83fa6bff8ad31f08e3586ef8f9c1c84e27b3bab7
Sumeyye Sisman <sumyssmn@gmail.com>
2025-02-08 18:11:27 +0300

    scanner class

--- src/main/java/com/sumeyyessmn/_1_part_javatypes/_08_Scanner.java
+++ src/main/java/com/sumeyyessmn/_1_part_javatypes/_08_Scanner.java
@@ -1,4 +1,26 @@
 package com.sumeyyessmn._1_part_javatypes;
 
+import java.util.Scanner;
+
 public class _08_Scanner {
+    public static void main(String[] args) {
+        //Field
+        String name, surname;
+        int language;
+        //kullanıcıdan veri almak istiyorsak
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen adınızı giriniz: ");
+        name = scanner.nextLine();
+        System.out.println("Lütfen soyadınızı giriniz: ");
+        surname = scanner.nextLine();
+        System.out.println("Bildiğiniz dil sayısı: ");
+        language = scanner.nextInt();
+
+        System.out.println("Adınız: "+name+" Soyadınız: "+surname+"\nBildiğiniz dil sayısı: "+language);
+        //kullanmadığım scanner nesnesini kapatıyorum
+        scanner.close();
+
+
+    }
 }
