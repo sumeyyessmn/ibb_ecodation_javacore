01d680e239464e4f11a5757faa21085ebb0e400e
Sumeyye Sisman <sumyssmn@gmail.com>
2025-02-09 15:39:20 +0300

    autoboxing unboxing

--- src/main/java/com/sumeyyessmn/_1_week/_04_4_AutoBoxing_Boxing.java
+++ src/main/java/com/sumeyyessmn/_1_week/_04_4_AutoBoxing_Boxing.java
@@ -1,4 +1,17 @@
 package com.sumeyyessmn._1_week;
 
 public class _04_4_AutoBoxing_Boxing {
+    public static void main(String[] args) {
+        //Autoboxing:Primitive => Wrapper type dönüştürmek
+        int primitiveValue = 100;
+        Integer wrapperValue = primitiveValue;
+        //wrapperValue=null;
+        System.out.println("primitive type: "+primitiveValue+" wrapper type: "+wrapperValue);
+
+        //Unboxing : Wrapper Type => Primitive type dönüştürmek
+        Integer wrapperValue2 = 200;
+        int primitiveValue2 = wrapperValue2; //unboxing
+        System.out.println("Wrapper type2: "+wrapperValue2+" Primitive type2: "+primitiveValue2);
+
+    }
 }
