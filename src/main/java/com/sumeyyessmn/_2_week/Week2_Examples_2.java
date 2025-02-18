76b51619b89613401bf70909360a0c4ec5f4c60b
Sumeyye Ssmn <sumeyye.ssmn@gmail.com>
2025-02-18 19:53:47 +0300

    Faktöriyel Validation

--- src/main/java/com/sumeyyessmn/_2_week/Week2_Examples_2.java
+++ src/main/java/com/sumeyyessmn/_2_week/Week2_Examples_2.java
@@ -23,42 +23,59 @@ public class Week2_Examples_2 {
             // Variables
             // result:1 vermeliyiz ki, 1 sayısı çarpmada etkisizdir ve başlangıç değerimizdir.
             long number, result = 1;
-            byte doubleRemainingRight = 3;
+            byte doubleRemainingRight = 3; // Ondalıklı sayı giriş hakkı
+            boolean isActive=true;
 
             // Scanner
             Scanner scanner = new Scanner(System.in);
 
             // Sonsuz Döngü
             while (true) {
-                System.out.println("\nLütfen pozitif bir sayı giriniz");
+                if (doubleRemainingRight > 0) {
+                    System.out.println("\nLütfen pozitif bir sayı giriniz");
 
-                // Kullanıcıdan ondalıklı sayı girip girmediğini kontrol edelim
-                if (scanner.hasNextInt()) {
-                    number = scanner.nextLong();
-                    if (number < 0) {
-                        System.out.println(SpecialColor.RED + "Sıfırdan küçük bir sayı girdiniz sistem kapatılıyor" + SpecialColor.RESET);
-                        //number = Math.abs(number);
-                        break;             // 1.YOL(Yalnızca bulunduğu döngü veya switch bloğu etkiliyor)
-                        //System.exit(0); // 2.YOL (JVM'yi kapatarak programı tamamem sonlandırıyor.)
-                    } else if (number == 0) {
-                        System.out.println(number + SpecialColor.BLUE + " sayısının faktöriyeli= 1");
-                        result = 1;
-                    } else {
-                        result = 1;
-                        for (int i = 1; i <= number; i++) {
-                            //result=result*i;
-                            result *= i;
+                    // Kullanıcıdan ondalıklı sayı girip girmediğini kontrol edelim
+                    if (scanner.hasNextInt()) {
+                        number = scanner.nextLong();
+                        if (number < 0) {
+                            System.out.println(SpecialColor.RED + "Sıfırdan küçük bir sayı girdiniz sistem kapatılıyor" + SpecialColor.RESET);
+                            //number = Math.abs(number);
+                            break;             // 1.YOL(Yalnızca bulunduğu döngü veya switch bloğu etkiliyor)
+                            //System.exit(0); // 2.YOL (JVM'yi kapatarak programı tamamem sonlandırıyor.)
+                        } else if (number == 0) {
+                            System.out.println(number + SpecialColor.BLUE + " sayısının faktöriyeli= 1");
+                            result = 1;
+                        } else {
+                            result = 1;
+                            for (int i = 1; i <= number; i++) {
+                                //result=result*i;
+                                result *= i;
+                            }
+                            System.out.println(number + SpecialColor.YELLOW + " sayısının " + number + "!=" + result + SpecialColor.RESET);
                         }
-                        System.out.println(number + SpecialColor.YELLOW + " sayısının " + number + "!=" + result + SpecialColor.RESET);
-                    }
-                } else if (scanner.hasNextDouble()) {
-                    System.out.println(SpecialColor.RED + "Ondalıklı bir sayı girdiniz" + SpecialColor.RESET);
-                    break;
+                    } else if (scanner.hasNextDouble()) {
+                        //doubleRemainingRight= (byte) (doubleRemainingRight-1);
+                        doubleRemainingRight--; // Best practice
+                        System.out.println(SpecialColor.RED + "Ondalıklı bir sayı girdiniz kalan hakkınız: "+doubleRemainingRight + SpecialColor.RESET);
+
+                        if(doubleRemainingRight ==0){
+                            System.out.println("üç defa ondalıklı sayı girdiğinizde dolayı Program sonlandı");
+                            isActive=false;
+                            System.exit(0);
+                        }
+                        scanner.next(); // Geçersiz girdileri temizlemek içindir
+                    } else {
+                        System.out.println(SpecialColor.RED + "Harf girdiniz." + SpecialColor.RESET);
+                        //break;
+                        scanner.next(); // Geçersiz girdileri temizlemek içindir
+                    } //end harf
                 } else {
-                    System.out.println(SpecialColor.RED + "Harf girdiniz." + SpecialColor.RESET);
-                    break;
-                } //end harf
+                    System.out.println("Virgüllü sayı kalan hakkınız kalmadı ve program sonlanıyor(JVM) tarafından");
+                    System.exit(0);
+                }
             } //end while
+            // Scanner Close
+            scanner.close();
         }
     }
 }
