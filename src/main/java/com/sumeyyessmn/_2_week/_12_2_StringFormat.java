ab997b4321f9a10559d9170b7edac6476d3f33f2
Sumeyye Ssmn <sumeyye.ssmn@gmail.com>
2025-02-18 12:52:05 +0300

    formatter2

--- src/main/java/com/sumeyyessmn/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/sumeyyessmn/_2_week/_12_2_StringFormat.java
@@ -4,6 +4,13 @@ package com.sumeyyessmn._2_week;
 // d
 // f
 
+/**
+ * Garbarage Collectors:
+ * Yalnızca ama yalnızca kapalı olmayan dosya, scanner, formatter vb.
+ * yapılarda otomatik kapanmaz. Eğer biz bunu manuel olarak kapatmazsak
+ * bu kapatılmayan nesneyi temizleyebilir ancak açık olan dosyayı
+ * kapatmazsak cache belleği kullanmaya devam eder.
+ */
 import java.util.Formatter;
 
 public class _12_2_StringFormat {
@@ -13,10 +20,18 @@ public class _12_2_StringFormat {
         formatter.format("Merhabalar Adınız: %s, T.C.: %d, Fiyat: %f","Sumeyye",343563,44.56);
         System.out.println(formatter);
         formatter.close(); //Belleği serbest bırakmak için
+
+    }
+    //formatter2 (Best Practice)
+    public static void formatter2(){
+        //Formatter formatter = new Formatter();
+        String formatterString = String.format("Merhabalar Adınız: %s, T.C.: %d, Fiyat: %f","Sumeyye",343563,44.56);
+        System.out.println(formatterString);
     }
 
     public static void main(String[] args) {
-        formatter1();
+        //formatter1();
+        formatter2();
     }
     
 }
